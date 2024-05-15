54df3be395fac229038790f72dab50eed2edd5c3
Jayandhan R <jayandhanrajendran03@gmail.com>
2024-05-15 08:22:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const app = express();
 const appRoute = require('./routes/route.js');
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const cors = require('cors'); // Import cors module
+
+// Enable CORS for all routes
+app.use(cors());
+
+
+// Define OPTIONS route for preflight requests
+app.options('/api/product/getbill', (req, res) => {
+    console.log('Received preflight request for /api/product/getbill');
+    res.set('Access-Control-Allow-Origin', '*'); // Set Access-Control-Allow-Origin header
+    res.status(200).end();
+  });
+
+const PORT = process.env.PORT || 8000;
+
 
 app.use(express.json());
 
