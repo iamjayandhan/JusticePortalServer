d03059dc917d5502a0649184f72f384b0e6628b2
Jayandhan R <jayandhanrajendran03@gmail.com>
2024-05-16 19:08:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const app = express();
 const appRoute = require('./routes/route.js');
-const cors = require('cors'); // Import cors module
+const cors = require('cors');
 
 // Enable CORS for all routes
 app.use(cors());
-
-// Define OPTIONS route for preflight requests
-app.options('/api/product/getbill', (req, res) => {
-    console.log('Received preflight request for /api/product/getbill');
-    res.set('Access-Control-Allow-Origin', '*'); // Set Access-Control-Allow-Origin header
-    res.status(200).end();
-});
 
 const PORT = process.env.PORT || 5000;
 
@@ -19,6 +12,9 @@ app.use(express.json());
 
 /* routes */
 app.use('/api', appRoute);
+
+// Define OPTIONS route for preflight requests
+app.options('/api/product/getbill', cors()); // Use cors middleware for OPTIONS request
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
