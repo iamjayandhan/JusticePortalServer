f72cabca514a19f0eec278d216702110eabf8c4d
Jayandhan R <jayandhanrajendran03@gmail.com>
2024-05-16 15:50:38 +0530

    Update appController.js

--- controller/appController.js
+++ controller/appController.js
@@ -1,26 +1,11 @@
+// controller/appController.js
+
 const nodemailer = require('nodemailer');
 const Mailgen = require('mailgen');
-const { EMAIL, PASSWORD } = require('../env.js');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const { EMAIL, PASSWORD } = process.env;
 
 const signup = async (req, res) => {
     try {
-        /* TESTING ACCOUNT */
-        let testAccount = await nodemailer.createTestAccount();
-
-        /* create reusable transporter object using the default SMTP transport */
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            auth: {
-                user: "maddison53@ethereal.email",
-                pass: "jn7jnAPss4f63QBp6D",
-            },
-            tls: {
-                rejectUnauthorized: false // Accept self-signed certificates
-            },
-        });
-
         let message = {
             from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
             to: "bar@example.com, baz@example.com",
@@ -29,6 +14,18 @@ const signup = async (req, res) => {
             html: "<b>Successfully Register with us.</b>",
         };
 
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            auth: {
+                user: EMAIL,
+                pass: PASSWORD,
+            },
+            tls: {
+                rejectUnauthorized: false // Accept self-signed certificates
+            },
+        });
+
         const info = await transporter.sendMail(message);
         return res.status(201).json({
             msg: "You should receive an email",
@@ -36,66 +33,11 @@ const signup = async (req, res) => {
             preview: nodemailer.getTestMessageUrl(info)
         });
     } catch (error) {
-        console.error("Signup Error:", error); // Log the specific error
-        return res.status(500).json({ error: error.message }); // Return the error message
+        console.error("Signup Error:", error);
+        return res.status(500).json({ error: error.message });
     }
-};
-
-const getBill = (req, res) => {
-    const { userEmail, mailBody, subject } = req.body;
-
-    let config = {
-        service: 'gmail',
-        auth: {
-            user: EMAIL,
-            pass: PASSWORD
-        }
-    };
-
-    let transporter = nodemailer.createTransport(config);
-
-    let MailGenerator = new Mailgen({
-        theme: "default",
-        product: {
-            name: "Justice Portal Team",
-            link: 'https://justice-portal.vercel.app/',
-        }
-    });
-
-    let response = {
-        body: mailBody,
-    };
-    
-
-    // Check if userEmail is received
-    console.log('User Email:', userEmail);
-    let mail = MailGenerator.generate(response);
-
-    let message = {
-        from: EMAIL,
-        to: userEmail,
-        subject: subject || 'Registration Successful!',
-        html: mail
-    };
-
-    transporter.sendMail(message).then(() => {
-
-        // Set CORS headers in the response
-        res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-        // Optionally, you can set other CORS headers as needed
-        res.setHeader('Access-Control-Allow-Methods', 'POST');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-        return res.status(201).json({
-            msg: "You should receive an email"
-        });
-    }).catch(error => {
-        console.error("GetBill Error:", error); // Log the specific error
-        return res.status(500).json({ error: error.message }); // Return the error message
-    });
 };
 
 module.exports = {
     signup,
-    getBill,
 };
