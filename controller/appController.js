6da13e7b7632f9983708cc63cb250a611e6940c0
Jayandhan <jayandhanrajendran03@gmail.com>
2024-05-17 14:03:00 +0530

    appc4

--- controller/appController.js
+++ controller/appController.js
@@ -1,42 +1,3 @@
-const nodemailer = require('nodemailer');
-const Mailgen = require('mailgen');
-const { EMAIL, PASSWORD } = require('../env.js');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-const signup = async (req, res) => {
-    try {
-        /* create reusable transporter object using the default SMTP transport */
-        const transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: EMAIL,
-                pass: PASSWORD,
-            },
-            tls: {
-                rejectUnauthorized: false // Accept self-signed certificates
-            },
-        });
-
-        let message = {
-            from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
-            to: "bar@example.com, baz@example.com",
-            subject: "Hello ✔",
-            text: "Successfully Register with us.",
-            html: "<b>Successfully Register with us.</b>",
-        };
-
-        const info = await transporter.sendMail(message);
-        return res.status(201).json({
-            msg: "You should receive an email",
-            info: info.messageId,
-            preview: nodemailer.getTestMessageUrl(info)
-        });
-    } catch (error) {
-        console.error("Signup Error:", error); // Log the specific error
-        return res.status(500).json({ error: error.message }); // Return the error message
-    }
-};
-
 const getBill = (req, res) => {
     const { userEmail, mailBody, subject, userName } = req.body;
 
@@ -70,12 +31,12 @@ const getBill = (req, res) => {
         html: mail
     };
 
-    transporter.sendMail(message).then(() => {
-        // Set CORS headers in the response
-        res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-        res.setHeader('Access-Control-Allow-Methods', 'POST');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    // Set CORS headers in the response
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+    transporter.sendMail(message).then(() => {
         return res.status(201).json({
             msg: "You should receive an email"
         });
