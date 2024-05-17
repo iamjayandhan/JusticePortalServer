760a6b182dacaff587048770dfa86ddfac4e2886
Jayandhan R <jayandhanrajendran03@gmail.com>
2024-05-17 12:31:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,16 @@ const appRoute = require('./routes/route.js');
 const PORT = process.env.PORT || 5000;
 
 // Use CORS middleware
-app.use(cors());
+app.use(cors({
+    origin: '*', // Allow all origins for testing, you can restrict this to specific origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+}));
 
 app.use(express.json());
 
 // Define OPTIONS route for preflight requests
 app.options('*', (req, res) => {
-    console.log('Received preflight request');
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
