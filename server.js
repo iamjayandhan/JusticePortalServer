420c18734fa88574895dccb771373f932fe5111a
Jayandhan R <jayandhanrajendran03@gmail.com>
2024-05-17 12:16:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const appRoute = require('./routes/route.js');
 
 const PORT = process.env.PORT || 5000;
 
+// Use CORS middleware
+app.use(cors({
+    origin: '*', // You can restrict this to specific origins if needed
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+}));
+
 app.use(express.json());
 
 // Define OPTIONS route for preflight requests
-app.options('/api/product/getbill', (req, res) => {
-    console.log('Received preflight request for /api/product/getbill');
-    // Set CORS headers
+app.options('*', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -20,5 +26,6 @@ app.options('/api/product/getbill', (req, res) => {
 app.use('/api', appRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `http://localhost:${PORT}`;
+    console.log(`Server is running on ${baseUrl}`);
 });
