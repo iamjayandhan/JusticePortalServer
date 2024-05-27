ed70947c19a3f13bad200e62c3f04ee9a4f362a0
Jayandhan R <jayandhanrajendran03@gmail.com>
2024-05-27 21:57:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,15 @@ app.use(express.json());
 /* routes */
 app.use('/api', appRoute);
 
+// Define OPTIONS route for preflight requests
+app.options('/api/product/getbill', (req, res) => {
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', 'https://justice-portal.vercel.app'); // Allow requests from the specific origin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); // Allow the specified methods
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); // Allow the specified headers
+    res.sendStatus(200); // Respond with OK status
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
